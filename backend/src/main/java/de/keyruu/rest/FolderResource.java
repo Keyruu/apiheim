6ba12571b502c64bb7380f766c276ad5d4711075
Lucas Rott <lro@exentra.de>
2022-02-11 18:00:10 +0100

    🐛 Fix delete for folder

--- backend/src/main/java/de/keyruu/rest/FolderResource.java
+++ backend/src/main/java/de/keyruu/rest/FolderResource.java
@@ -10,8 +10,11 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import java.util.List;
 
 @Path("/folder")
@@ -34,12 +37,11 @@ public class FolderResource {
     return folder;
   }
 
+  @Path("{id}")
   @DELETE
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
   @Transactional
-  public Folder deleteFolder(Folder folder) {
-    folderRepository.delete(folder);
-    return folder;
+  public Response deleteApi(@PathParam("id") String id) {
+    folderRepository.deleteById(Long.valueOf(id));
+    return Response.noContent().build();
   }
 }
